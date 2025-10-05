c9f92425495298838bc1a9560c96e79a931d15a7
zj <3382865253@qq.com>
2025-10-05 16:39:38 +0800

    style：优化蛇的样式

--- JS/Box.js
+++ JS/Box.js
@@ -9,6 +9,7 @@ export default class Box {
   }
   render() {
     const box = document.createElement('div');
+    box.classList.add('box');
     box.style.width = `${this.width}px`;
     box.style.height = `${this.height}px`;
     box.style.backgroundColor = this.color;
